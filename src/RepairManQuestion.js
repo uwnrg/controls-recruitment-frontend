a553472d3d08f36e0862f2927b4ef2a0e38a80d5
Jeff Niu <jeffniu22@gmail.com>
2018-05-30 13:32:57 -0400

    Update RepairManQuestion.js

--- src/RepairManQuestion.js
+++ src/RepairManQuestion.js
@@ -20,9 +20,9 @@ export default class RepairManQuestion extends React.Component {
                     Jimmy Neutron needs to repair his robots. He has <code>num_robots</code> spaced <b>one meter</b> apart
                     along a line in his (very long) garage. Jimmy starts one meter to the left of the first robot, where he
                     keeps his stash of spare parts. Jimmy needs spare parts to repair his robots, but he can
-                    only carry <b>50 at a time</b>. Each of the robots requires a certain number of parts to repair. These
-                    are given in the array <code>spare_parts_needed</code>. Jimmy can resupply his parts by returning to his
-                    start position.
+                    only carry <b>50 at a time</b>. Each of the robots requires a certain number of parts to repair, but each
+                    number will be less than or equal to 50. These are given in the array <code>spare_parts_needed</code>. 
+                    Jimmy can resupply his parts by returning to his start position.
                 </p>
                 <p>
                     The function <code>repair_order</code> should return the order in which Jimmy should
@@ -38,9 +38,10 @@ export default class RepairManQuestion extends React.Component {
                     for a total of 16 meters travelled.
                 </p>
                 <p>
-                    You can chsnge the array and vector to whichever array and list types are available 
+                    You can change the array and vector to whichever array and list types are available 
                     in your language of choice, e.g. <code>int[] spare_parts_needed</code> and 
-                    <code>List<Integer></code> in Java.
+                    <code>List<Integer></code> in Java. If you're not familiar with lists, 
+                    use an array with size <code>2 * num_robots</code>.
                 </p>
             </CodeQuestion>
         );
